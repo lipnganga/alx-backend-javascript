8ec5c176b0570b4686bbefad486f2df761b2fc6f
lipnganga <philipmurage98@gmail.com>
2023-06-14 22:28:17 +0300

    soln to task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,17 +1,13 @@
-//small express server
 const express = require('express');
 
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
-    }
-);
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-    }
-);
+});
 
 module.exports = app;
